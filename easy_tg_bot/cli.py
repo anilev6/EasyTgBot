9940132c2de5932d82f45a75fc8941a9ad6dce97
ani <125677472+anilev6@users.noreply.github.com>
2024-08-23 16:47:24 +0200

    upd

--- easy_tg_bot/cli.py
+++ easy_tg_bot/cli.py
@@ -77,6 +77,7 @@ def vultr():
     root_dir = os.getcwd()
     initialize_file_from_draft("Dockerfile", root_dir)
     initialize_file_from_draft(".dockerignore", root_dir)
+    initialize_file_from_draft(".docker-compose.yml", root_dir)
     create_vultr_deploy_yml_from_draft()
 
 
